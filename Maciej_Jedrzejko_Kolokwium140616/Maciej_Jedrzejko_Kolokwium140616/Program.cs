8fe463c605c5bf69eeb178788969488990d771cd
Maciej Jedrzejko <Maciej Jedrzejko>
2016-06-14 16:54:07 +0200

    Resolved bug causing method to check only cars next to eachother not all cars in the arrays with all cars. Added todo comment about too many passes funcionality of the method

--- Maciej_Jedrzejko_Kolokwium140616/Maciej_Jedrzejko_Kolokwium140616/Program.cs
+++ Maciej_Jedrzejko_Kolokwium140616/Maciej_Jedrzejko_Kolokwium140616/Program.cs
@@ -12,17 +12,21 @@ namespace Maciej_Jedrzejko_Kolokwium140616
         {
             int valueOfCars = 0;
             int N = 50;
-            for(int i=0;i<50;i++)
+            for(int i=0;i<N;i++)
             {
-                if (a[i] < a[i + 1])
-                   valueOfCars ++; 
+                for (int j=0;j<N;j++)
+                {
+                    if (a[i] < a[j])
+                        valueOfCars++;
+                }
+                 
             }
             //Todo checking whole array, not only next car in it
             
             return valueOfCars;
         }
         //Todo method Cars(int[] a) resolving problem given on paper.
-
+        //Todo if theres more then 1 000 000 000 valueOfCars = -1
          
 
         public static void Main(string[] args)
