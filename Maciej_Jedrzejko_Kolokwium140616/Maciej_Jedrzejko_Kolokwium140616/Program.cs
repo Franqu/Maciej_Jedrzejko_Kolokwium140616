91fc8c82897f4ea98e11abcfd091c16e5fad4b3e
Maciej Jedrzejko <Maciej Jedrzejko>
2016-06-14 16:23:13 +0200

    Created a project

---
+++ Maciej_Jedrzejko_Kolokwium140616/Maciej_Jedrzejko_Kolokwium140616/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maciej_Jedrzejko_Kolokwium140616
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
